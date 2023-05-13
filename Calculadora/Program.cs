7a247704dd774a08d5e78eae8c3e8cb0f2207b5c
Luiz Henrique Penga <luuizhpenga@gmail.com>
2023-05-13 00:28:37 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("*************************************");
+Console.WriteLine("************ CALCULADORA ************");
+Console.WriteLine("*************************************\n");
+
+Console.WriteLine("Digite o caractere desejado:(+ - * /)");
+char caracter = char.Parse(Console.ReadLine());
+Console.WriteLine("Digite o primeiro numero: ");
+double n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo numero: ");
+double n2 = double.Parse(Console.ReadLine());
+
+if(caracter == '+')
+{
+    Console.WriteLine($"A soma do primeiro e segundo número é: { n1+n2}\n\n");
+}
+if(caracter == '-')
+{
+    Console.WriteLine($"A subtração de primeiro e segundo número é: {n1-n2}\n\n");
+}
+if(caracter == '*')
+{
+    Console.WriteLine($"A multiplicação do primeiro e segundo número é: {n1 * n2}\n\n");
+}
+else if(caracter == '/')
+{
+    Console.WriteLine($"A divisão dos primeiro e segundo número é: {n1/n2}\n\n");
+}
+
+
+Console.WriteLine("********* Project by Penga **********\n");
